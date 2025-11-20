d1bcf06d3c03c436615e96b782d806f949ebb5c7
razzat008 <93041325+razzat008@users.noreply.github.com>
2025-11-20 21:41:04 +0545

    added: description of instruction ADD

--- src/main.c
+++ src/main.c
@@ -22,6 +22,8 @@ void handle_interrupt(int signal) {
   printf("\n");
   exit(-2);
 }
+uint16_t sign_extend(uint16_t, int);
+void update_flags(uint16_t);
 int main(int argc, char *argv[]) {
 
   signal(SIGINT, handle_interrupt);
@@ -51,17 +53,53 @@ int main(int argc, char *argv[]) {
     /*
      Extracting the opcode from the instrution, as remaning 4 bits after
      shifting usually is representation of an opcode.
-       15            12 11                         0
-       +---------------+----------------------------+
-       |   OPCODE      |         Operands           |
-       +---------------+----------------------------+
+    Register Addressing Mode:
+       15            12 11   9 8   6  5  4  3 2    0
+       +---------------+------+-----+---+----+------+
+       |   OPCODE      |  DR  | SR1 | 0 | 00 | SR2  |
+       +---------------+------------+---+----+------+
+       SR1 -> first number to add
+       SR2 -> second number to add
+       DR -> destination register
+
+    Immediate Addressing Mode:
+       15            12 11   9 8   6  5  4         0
+       +---------------+------+-----+---+-----------+
+       |   OPCODE      |  DR  | SR1 | 1 |    imm5   |
+       +---------------+------------+---+-----------+
+
+       So, kinda like
+       if bit[5] == 0 {
+       DR = SR1 + SR2;
+       } else if bit[5] == 1{
+       DR =  SR1 + SEXT(imm5)
+       }
+
+
       */
     uint16_t op = instr >> 12;
 
     switch (op) {
     case OP_ADD: {
-        // as register mode(using registers to pass around values) and immediate mode(using values directly ) both are supported
-      uint16_t r0 = (instr >> 9) & 0x7; 
+      /* as register mode(using registers to pass around values) and immediate
+      mode(using values directly ) both are supported
+      the destination register as DR lies on the 9-11th position (15 MSB)*/
+      uint16_t r0 = (instr >> 9) & 0x7;
+      // first operand
+      uint16_t r1 = (instr >> 6) & 0x7;
+      // checking for immediate mode
+      uint16_t imm_flag = (instr >> 5) & 0x1;
+
+      if (imm_flag) {
+        uint16_t imm5 = sign_extend(instr & 0x1F, 5); // 0x1F is 0001 1111
+        reg[r0] = reg[r1] + imm5;
+      } else {
+
+        uint16_t r2 = instr & 0x7;
+        reg[r0] = reg[r1] + reg[r2];
+      }
+      update_flags(r0);
+
       break;
     }
 
@@ -130,3 +168,21 @@ int main(int argc, char *argv[]) {
   restore_input_buffering();
   return EXIT_SUCCESS;
 }
+
+uint16_t sign_extend(uint16_t x, int bit_count) {
+  // checking if the 7th bit is 1; and if it is, then
+  if ((x >> (bit_count - 1)) & 1) {
+    x |= (0xFFFF << bit_count);
+  }
+  return x;
+}
+
+// updating flags on the basis of result obtained when doing arithmetics
+void update_flags(uint16_t r) {
+  if (reg[r] == 0) {
+    reg[R_COND] = FL_ZRO;
+  } else if (reg[r] >> 15) {
+    reg[R_COND] = FL_NEG;
+  } else {
+    reg[R_COND] = FL_POS;
+  }
